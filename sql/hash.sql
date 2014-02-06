9e0a1ab088d6c8d73773708632d3972e5a88d705
rahmanhamed <email4.m.rahman@gmail.com>
2014-02-06 17:26:31 +0200

    DB Changes, Scrapesource_hash table creation

--- sql/hash.sql
+++ sql/hash.sql
@@ -1,2 +1,11 @@
  ALTER TABLE Scrapesource ADD COLUMN category_id INT NOT NULL ; 
- ALTER TABLE Scrapesource ADD COLUMN hash varchar(1024);
+
+CREATE TABLE `Scrapesource_hash` (
+  `hash_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `scrapesource_id` int(10) unsigned NOT NULL,
+  `hash` varchar(32) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime DEFAULT NULL,
+  `deleted` datetime DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`hash_id`)
