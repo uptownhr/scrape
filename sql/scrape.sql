ad0fe0a05030c314c1826a865ae18658269eca15
rahmanhamed <email4.m.rahman@gmail.com>
2014-02-07 17:39:04 +0200

    Copying latest modifications from sql/hash.sql to sql/scrape.sql

--- sql/scrape.sql
+++ sql/scrape.sql
@@ -360,3 +360,16 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2014-01-29 18:46:58
+ ALTER TABLE Scrapesource ADD COLUMN category_id INT NOT NULL ; 
+
+
+CREATE TABLE `Scrapesource_hash` (
+  `hash_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `scrapesource_id` int(10) unsigned NOT NULL,
+  `hash` varchar(32) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime DEFAULT NULL,
+  `deleted` datetime DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`hash_id`)
+	
